85a513b47abd3db4bca409efe5197897e643649f
ajkn1904 <ajknishat@gmail.com>
2022-12-29 19:20:33 +0600

    update

--- src/Components/Shared/Footer/Footer.js
+++ src/Components/Shared/Footer/Footer.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Footer = () => {
     return (
-        <div>
+        <div className='w-full h-20 flex justify-center items-center bg-slate-600 text-white'>
             <small className='text-center'>© 2022 || Anika Jumana Khanam</small>
         </div>
     );
